f757133d3e29ba2139f9c391228e4ae070fb6269
hashdila <dilannawa@gmail.com>
2024-03-18 08:06:23 +0530

    view

--- src/cr-prototype/cr-prototype.service.ts
+++ src/cr-prototype/cr-prototype.service.ts
@@ -4,30 +4,45 @@ import { UpdateCrPrototypeDto } from './dto/update-cr-prototype.dto';
 import { CRPrototype } from './cr-prototype.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CR } from '../chngerequest/chngerequest.entity'
 
 
 @Injectable()
-export class CrPrototypeService {
-  create(createDdDto: CreateCrPrototypeDto) {
-    return 'This action adds a new dd';
-  }
+export class CRPrototypeService {
+  constructor(
+    @InjectRepository(CRPrototype)
+    private readonly crPrototypeRepository: Repository<CRPrototype>,
+    @InjectRepository(CR)
+    private readonly CrRepository: Repository<CR>,
+  ) {}
+
+  async createCRPrototype(file: string, description: string, crId: number): Promise<CRPrototype> {
+    const crPrototype = new CRPrototype();
+    crPrototype.file = file;
+    crPrototype.discription = description;
     
+    // Find the CR by its ID and assign it to the cr property
+    crPrototype.cr = await this.CrRepository.findOneOrFail({ where: { crId } });
 
-
-
-  findAll() {
-    return `This action returns all crPrototype`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} crPrototype`;
-  }
-
-  update(id: number, updateCrPrototypeDto: UpdateCrPrototypeDto) {
-    return `This action updates a #${id} crPrototype`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} crPrototype`;
+    return await this.crPrototypeRepository.save(crPrototype);
   }
 }
+
+
+
+  // findAll() {
+  //   return `This action returns all crPrototype`;
+  // }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} crPrototype`;
+  // }
+
+  // update(id: number, updateCrPrototypeDto: UpdateCrPrototypeDto) {
+  //   return `This action updates a #${id} crPrototype`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} crPrototype`;
+  // }
+// }
